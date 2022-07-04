fa0977da88a71506c1875165417e658547188b87
rosenrose <crazytempler@gmail.com>
2022-07-04 17:38:53 +0900

    표준 출력

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -10,6 +6,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            foreach (var arg in args)
+            {
+                Console.Write(arg);
+            }
+            Console.WriteLine(true);
+            Console.WriteLine("{0} {1}", 12, 3.14f);
+            Console.WriteLine("{0} + {1} = {2}", 1, 2, 1 + 2);
+            Console.WriteLine("{0:C} {1:P} {0:X} {0:G}", 123, 123.45);
         }
     }
 }
