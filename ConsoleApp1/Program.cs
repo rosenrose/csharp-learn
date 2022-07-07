4d02f221bae15b3b8241542314ddaf6582844b95
rosenrose <crazytempler@gmail.com>
2022-07-07 12:36:09 +0900

    표준 입력

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,32 +6,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var a = 3.14f;
-            float b = 10.0f;
-            Console.WriteLine("{0} {1:f0} {2:f1}", a, b, a + b);
+            Console.WriteLine("{0} {1}", Console.Read(), Console.Read());
 
-            int? c = null;
-            string d = null;
-            Console.WriteLine("{0} {1} {2} {3}", c, c.HasValue, c == null, d);
+            ConsoleKeyInfo KeyInfo;
+            do
+            {
+                KeyInfo = Console.ReadKey(true);
+                Console.WriteLine("{0} {1} {2}", KeyInfo.Key, KeyInfo.KeyChar, KeyInfo.Modifiers);
+            } while (KeyInfo.Key != ConsoleKey.Escape);
+            Console.WriteLine(Console.ReadLine());
 
-            var Var1 = a.ToString();
-            var Var2 = double.Parse(Var1);
-            var Var3 = Convert.ToSingle(Var2);
-            Console.WriteLine("{0} {1}", Var2, Var3);
+            int Kor, Eng, Math, Total;
+            float Avg;
 
-            int m = 123;
-            object obj = m;
-            m = 30;
-            int n = (int)obj;
-            obj = "zz";
-            obj = true;
-            obj = 3.14;
-            Console.WriteLine("{0} {1} {2}", m, n, obj);
-
-            int i = 10;
-            int k = i;
-            i = 20;
-            Console.WriteLine(k);
+            Console.Write("국어: ");
+            Kor = int.Parse(Console.ReadLine());
+            Console.Write("영어: ");
+            Eng = int.Parse(Console.ReadLine());
+            Console.Write("수학: ");
+            Math = int.Parse(Console.ReadLine());
+            Total = Kor + Eng + Math;
+            Avg = Total / 3.0f;
+            Console.WriteLine("{0} {1} {2} {3} {4:f1}", Kor, Eng, Math, Total, Avg);
         }
     }
 }
