02a73806821fda76232d315944de18ef33215542
rosenrose <crazytempler@gmail.com>
2022-07-08 11:33:29 +0900

    3. 구조체, 클래스, 열거형

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,30 +4,63 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public struct Struct
+        {
+            public const int a = 0;
+            public static int b = 1;
+            public int Val;
+
+            public Struct(int Val_)
+            {
+                Val = Val_;
+            }
+        }
+
+        class Class
+        {
+            public int Val;
+        }
+
+        public struct Score
+        {
+            public int Kor, Eng, Math, Total;
+            public float Avg;
+
+            public void Compute()
+            {
+                Total = Kor + Eng + Math;
+                Avg = Total / 3.0f;
+            }
+        }
+
+        enum Days : byte { Sun = 0, Mon, Tue, Wed, Thu, Fri, Sat }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("{0} {1}", Console.Read(), Console.Read());
+            Struct s;
+            s.Val = 5;
+            Console.WriteLine($"{Struct.a} {Struct.b} {s} {s.Val}");
 
-            ConsoleKeyInfo KeyInfo;
-            do
-            {
-                KeyInfo = Console.ReadKey(true);
-                Console.WriteLine("{0} {1} {2}", KeyInfo.Key, KeyInfo.KeyChar, KeyInfo.Modifiers);
-            } while (KeyInfo.Key != ConsoleKey.Escape);
-            Console.WriteLine(Console.ReadLine());
+            Struct s2 = new Struct();
+            Struct s3 = s2;
+            s3.Val = 20;
+            Console.WriteLine($"{s2.Val} {s3.Val}");
 
-            int Kor, Eng, Math, Total;
-            float Avg;
+            Class c = new Class();
+            Console.WriteLine($"{c} {c.Val}");
+            Class c2 = c;
+            c.Val = 15;
+            Console.WriteLine($"{c.Val} {c2.Val}");
 
-            Console.Write("국어: ");
-            Kor = int.Parse(Console.ReadLine());
-            Console.Write("영어: ");
-            Eng = int.Parse(Console.ReadLine());
-            Console.Write("수학: ");
-            Math = int.Parse(Console.ReadLine());
-            Total = Kor + Eng + Math;
-            Avg = Total / 3.0f;
-            Console.WriteLine("{0} {1} {2} {3} {4:f1}", Kor, Eng, Math, Total, Avg);
+            Score score = new Score();
+            score.Kor = 80;
+            score.Eng = 77;
+            score.Math = 19;
+            score.Compute();
+            Console.WriteLine($"총점: {score.Total}, 평균: {score.Avg:f1}");
+
+            Days Wed = Days.Wed;
+            Console.WriteLine($"{Wed} {(int)Wed}");
         }
     }
 }
