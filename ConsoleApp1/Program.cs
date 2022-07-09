fb92d864e3b32610ea89a9dd98e4b5ea3b504f2f
rosenrose <crazytempler@gmail.com>
2022-07-09 09:01:32 +0900

    .net framework -> .net 6.0

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,19 +1,10 @@
-using System;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            int a = 2;
-            int b = new int();
-            Console.WriteLine($"{a} {b}");
-
-            int[] array = { 1, 2, 3 };
-            int[] array2 = array;
-            array2[1] = 9;
-            Console.WriteLine($"{array[0]} {array[1]} {array[2]}");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
