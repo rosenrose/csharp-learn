ec7549d6731cadf89908251be59aa1f6f69b82cd
rosenrose <crazytempler@gmail.com>
2022-07-11 15:30:02 +0900

    5. 다차원, 가변 배열

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,44 +1,53 @@
-using System.Collections;
-
 namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int Val = 15;
+            int[] arr = { 1, 2 };
+            Console.WriteLine(arr.Count());
 
-            switch (Val)
+            int[,] arr2 = new int[3, 2];
+            Console.WriteLine($"{arr2[0, 0]} {arr2.Length} {arr2.Rank} {arr2.GetLength(0)} {arr2.GetType()}");
+
+            int[,] arr3 = { { 1, 2 }, { 3, 4 } };
+            foreach (var i in arr3)
             {
-                case < 10:
-                    Console.WriteLine(Val + 10);
-                    break;
-                case > 10:
-                    Console.WriteLine(Val - 10);
-                    break;
+                Console.WriteLine(i);
             }
 
-            ArrayList List = new();
-            List.Add(1);
-            List.Add("a");
-            List.Add(true);
-
-            foreach (var i in List)
+            for (int i = 0; i < arr3.Rank; i++)
             {
-                Console.WriteLine($"{i} {i.GetType()}");
+                for (int j = 0; j < arr3.GetLength(0); j++)
+                {
+                    Console.WriteLine($"val: {arr3[i, j]} {i} {j}");
+                }
             }
 
-            try
+            int[][] arr4 = { new int[] { 1, 2 }, new int[] { 4, 6, 8 } };
+            Console.WriteLine($"{arr4.Length} {arr4.Rank} {arr4.GetLength(0)} {arr4.GetType()} {arr4.Count()}");
+            foreach (var (row, i) in arr4.Select((v, i) => (v, i)))
             {
-                int i = new[] { 1, 2 }[-2];
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"{e.Message} {e.StackTrace}");
-                Console.WriteLine(new[] { 1, 2 }[^2]);
+                foreach (var (value, j) in row.Select((v, i) => (v, i)))
+                {
+                    Console.WriteLine($"val: {value} {i} {j}");
+                }
             }
 
-            throw new Exception("zz");
+            int[][][] arr5 = {
+                new int[][] {
+                    new int[] { 1, 2 },
+                    new int[] { 4, 6, 8 }
+                },
+                new int[][] {
+                    new int[] { 1 },
+                    new int[] { 10, 10, 11, 7 }
+                }
+            };
+            int[][,] arr6 = {
+                new int[,] { { 1 }, { 2 } },
+                new int[,] { { 8 }, { 9 } }
+            };
         }
     }
 }
