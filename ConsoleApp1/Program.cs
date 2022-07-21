12789c647d3e728ee97d6f96fc0992b3e5190458
rosenrose <crazytempler@gmail.com>
2022-07-21 17:01:28 +0900

    8. 클래스

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,41 +1,32 @@
-using System.Text.Encodings.Web;
-using System.Text.Json;
-using System.Text.Unicode;
-
 namespace ConsoleApp1
 {
     internal class Program
     {
-        [Serializable]
-        struct Data
+        class Date
         {
-            public int Var1 { get; set; }
-            public double Var2 { get; set; }
-            public string Var3 { get; set; }
-            [NonSerialized] public bool Var4;
-            public Data(int Var1_, double Var2_, string Var3_) => (Var1, Var2, Var3, Var4) = (Var1_, Var2_, Var3_, true);
+            private int Year;
+            private int month;
+            public int Month
+            {
+                get => month * 2;
+                set => month = value - 1;
+            }
+            public int Day { get; set; } = 20;
+            public int Hour { get; }
+            public int Minute;
+            public void Print()
+            {
+                Console.WriteLine($"{Year} {Month} {Day}");
+            }
+            public Date(int day, int min) => Day = day;
         }
-
         static void Main(string[] args)
         {
-            List<Data> list = new()
-            {
-                new(3, 5.14, "world"),
-                new(10, 9.0, "ㅎㄴ거카"),
-            };
-
-            var options = new JsonSerializerOptions
-            {
-                //WriteIndented = true,
-                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
-            };
-            File.WriteAllText("test.txt", JsonSerializer.Serialize(list, options));
-
-            list = JsonSerializer.Deserialize<List<Data>>(File.ReadAllText("test.txt"))!;
-            foreach (var data in list)
-            {
-                Console.WriteLine($"{data.Var1} {data.Var2} {data.Var3} {data.Var4}");
-            }
+            Date d = new(min: 10, day: 15);
+            //Console.WriteLine($"{d.Year}");
+            d.Month = 9;
+            Console.WriteLine($"{d.Month} {d.Day}");
+            //d.Hour = 10;
         }
     }
 }
