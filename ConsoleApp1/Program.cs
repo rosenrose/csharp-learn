a0bae268c4e8b916f8a508b6122373eed6f98954
rosenrose <crazytempler@gmail.com>
2022-07-27 12:31:36 +0900

    11. 델리게이트와 이벤트

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,61 +2,30 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        delegate void CustomDelegate(string s);
         class A
         {
-            private int num;
-            public int Num
+            public void Print(string str)
             {
-                get => num; set => num = value;
+                Console.WriteLine(str);
             }
-            public void print()
+            public void Print2(string _)
             {
-                num = 10;
-                Num = 20;
-                Console.WriteLine($"{Num} {Num.GetType()}");
-            }
-            private int[] number = { 1, 2, 3 };
-            public int this[int index]
-            {
-                get
-                {
-                    if (index < -1 * number.Length || index >= number.Length)
-                    {
-                        throw new IndexOutOfRangeException();
-                    }
-
-                    return index < 0 ? number[index + number.Length] : number[index];
-                }
-                set
-                {
-                    if (index < -1 * number.Length || index >= number.Length)
-                    {
-                        throw new IndexOutOfRangeException();
-                    }
-
-                    if (index < 0)
-                    {
-                        index += number.Length;
-                    }
-
-                    number[index] = value;
-                }
+                Console.WriteLine("ㅋㅋ");
             }
         }
-
         static void Main(string[] args)
         {
-            int[,] a = { { 1, 2, }, { 3, 4 } };
-            int[][] b = { new int[] { 1, 2, 3 }, new int[] { 4 } };
+            var del = delegate (int x) { return x * x; };
+            Func<int, int> lambda = x => x * x;
 
-            A[] c = { new(), new(), new() };
-            c[0].print();
+            Console.WriteLine($"{lambda(2)} {del(4)}");
 
-            c[1][-1] = 10;
-            Console.WriteLine($"{c[1][-2]} {c[1][-1]}");
-
-            //c[1][-4] = 99;
-            Console.WriteLine(c[1][3]);
+            A a = new();
+            CustomDelegate p = a.Print;
+            p("a");
+            p += a.Print2;
+            p("b");
         }
     }
 }
