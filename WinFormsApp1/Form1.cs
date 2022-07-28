7ff43cbdb5a7924626951f14f37270f52c01206f
rosenrose <crazytempler@gmail.com>
2022-07-28 09:55:15 +0900

    11. Forms

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,19 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Hello";
+        }
+        private void Show(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi");
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            Button button1 = (Button)sender;
+            button1.Content = "Hello";
+            MessageBox.Show("Hi");
+        }
+    }
+}
