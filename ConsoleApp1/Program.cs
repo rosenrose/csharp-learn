04fb57bde765379fd00a0f59eaa95516cf64a0a8
rosenrose <crazytempler@gmail.com>
2022-07-29 13:12:26 +0900

    12. 다형성

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,42 +4,21 @@ namespace ConsoleApp1
     {
         class A
         {
-            private void PrintPrivate() => Console.WriteLine("A private");
-            protected void PrintProtected() => Console.WriteLine("A protected");
-            public virtual void PrintPublic() => Console.WriteLine("A public");
-            protected int Num;
-            public A(int num)
-            {
-                Num = num;
-                Console.WriteLine("A Constructor");
-            }
-            ~A() => Console.WriteLine("A finalizer");
+            public virtual void Print() => Console.WriteLine("A print");
         }
         class B : A
         {
-            public override void PrintPublic() => Console.WriteLine("B public");
-            public void Print()
-            {
-                //PrintPrivate();
-                PrintProtected();
-                PrintPublic();
-            }
-            private int Num;
-            public B(int num) : base(num)
-            {
-                Num = num - 5;
-                Console.WriteLine($"B Constructor {base.Num} {Num}");
-            }
-            ~B() => Console.WriteLine("B finalizer");
+            public override void Print() => Console.WriteLine("B print");
+            public int num = 3;
         }
         static void Main(string[] args)
         {
-            B b = new(10);
-            b.Print();
+            A a = new();
+            a.Print();
 
-            A a = b;
-            //a.Print();
-            a.PrintPublic();
+            A a2 = new B();
+            a2.Print();
+            // Console.WriteLine(a2.num);
         }
     }
 }
