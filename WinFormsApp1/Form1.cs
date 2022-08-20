609892e26525d33fcafae90b395359aae825bee0
rosenrose <crazytempler@gmail.com>
2022-08-20 14:43:27 +0900

    19. 비트맵

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -2,15 +2,25 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        Bitmap bitmap;
         public Form1()
         {
             InitializeComponent();
+
+            bitmap = new(400, 300);
+            SetClientSizeCore(400, 300);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            Image image = Properties.Resources.image;
-            e.Graphics.DrawImage(image, 100, 100);
+            //Image imgage = Image.FromFile("image.png");
+            Graphics graphics = Graphics.FromImage(bitmap);
+            graphics.Clear(Color.Yellow);
+
+            graphics.DrawString("C#", Font, Brushes.Black, 10, 10);
+            graphics.DrawRectangle(Pens.Red, 100, 10, 200, 100);
+
+            e.Graphics.DrawImage(bitmap, 0, 0);
         }
     }
 }
