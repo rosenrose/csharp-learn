cbe77dae3903fadca697e68ccc2352499a76fec0
rosenrose <crazytempler@gmail.com>
2022-08-22 14:34:28 +0900

    22. 텍스트 박스

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -7,20 +7,9 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            string msg = $"check1: {checkBox1.Checked}, check2: {checkBox2.CheckState}";
-            MessageBox.Show(msg);
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            MessageBox.Show(((CheckBox)sender).Checked.ToString());
-        }
-
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-            MessageBox.Show(((RadioButton)sender).Checked.ToString());
+            MessageBox.Show(((TextBox)sender).Text);
         }
     }
 }
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -11,34 +11,11 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
-
-            RadioButton1.IsChecked = null;
         }
 
-        private void OnButtonClick(object sender, RoutedEventArgs e)
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-            string msg = $"check1: {(CheckBox1.IsChecked == null ? "indeterminate" : CheckBox1.IsChecked)}, check2: {CheckBox2.IsChecked}";
-            MessageBox.Show(msg);
-        }
-
-        private void OnCheckBoxChecked(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(((CheckBox)sender).IsChecked.ToString());
-        }
-
-        private void OnCheckBoxUnchecked(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(((CheckBox)sender).IsChecked.ToString());
-        }
-
-        private void OnRadioButtonChecked(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(((RadioButton)sender).IsChecked.ToString());
-        }
-
-        private void OnRadioButtonUnchecked(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(((RadioButton)sender).IsChecked.ToString());
+            MessageBox.Show(((TextBox)sender).Text);
         }
     }
 }
