1a61a9ed3f64bc35b2da460826a53f19426447f0
rosenrose <crazytempler@gmail.com>
2022-08-23 18:36:35 +0900

    22. DateTime, Calendar

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -5,17 +5,61 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
+
+            dateTimePicker4.CustomFormat = "hh:mm tt yyyy/MM/dd";
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
-            pictureBox2.Image = Properties.Resources.image2;
+            if (timer1.Enabled)
+            {
+                return;
+            }
+
+            timer1.Start();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            pictureBox1.Image = Properties.Resources.image2;
+            if (progressBar1.Value >= 100)
+            {
+                timer1.Stop();
+                progressBar1.Value = 0;
+                progressBar2.Value = 0;
+                progressBar3.Value = 0;
+
+                return;
+            }
+
+            //progressBar1.PerformStep();
+            progressBar1.Value++;
+            progressBar2.Value++;
+            progressBar3.Value++;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            label1.Text = dateTimePicker1.Value.ToString();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            label1.Text = dateTimePicker2.Value.ToShortDateString();
+        }
+
+        private void dateTimePicker3_ValueChanged(object sender, EventArgs e)
+        {
+            label1.Text = dateTimePicker3.Value.ToShortTimeString();
+        }
+
+        private void dateTimePicker4_ValueChanged(object sender, EventArgs e)
+        {
+            label1.Text = dateTimePicker4.Value.ToLongDateString();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            label1.Text = e.Start.ToLongTimeString();
         }
     }
 }
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,15 +1,85 @@
+using System;
+using System.ComponentModel;
+using System.Globalization;
 using System.Windows;
+using System.Windows.Data;
+using System.Windows.Threading;
 
 namespace WpfApp1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void RaisePropertyChanged(string? propname = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propname));
+        }
+
+        public double progress;
+        public double Progress
+        {
+            get => progress;
+            set
+            {
+                if (progress != value)
+                {
+                    progress = value;
+                    RaisePropertyChanged("Progress");
+                }
+            }
+        }
+        private DispatcherTimer timer = new()
+        {
+            Interval = TimeSpan.FromSeconds(0.01)
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+            timer.Tick += TimerTick;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (timer.IsEnabled)
+            {
+                return;
+            }
+
+            timer.Start();
+        }
+
+        private void TimerTick(object? sender, EventArgs e)
+        {
+            if (Progress >= 100)
+            {
+                timer.Stop();
+                Progress = 0;
+                return;
+            }
+
+            Progress++;
+        }
+    }
+
+    public class DateTimeConverter : IMultiValueConverter
+    {
+        public object Convert(object[] value, Type targetType, object param, CultureInfo culture)
+        {
+            string Date, Time;
+            Date = value[0] == null ? "" : ((DateTime)value[0]).ToLongDateString();
+            Time = value[1] == null ? "" : ((DateTime)value[1]).ToShortTimeString();
+
+            return $"{Date} / {Time}";
+        }
+        public object[] ConvertBack(object value, Type[] targetTypes, object param, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
