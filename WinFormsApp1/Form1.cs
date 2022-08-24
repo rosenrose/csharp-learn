8a8d4769599eb6b8a74124d51e04fd10750e42fe
rosenrose <crazytempler@gmail.com>
2022-08-24 18:10:26 +0900

    23. 리스트 뷰

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -2,19 +2,110 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        private TextBox[] inputBoxes;
+
         public Form1()
         {
             InitializeComponent();
+
+            inputBoxes = new TextBox[] { textBox1, textBox2, textBox3 };
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show($"{tabControl1.SelectedIndex} {tabControl1.SelectedTab}");
+            ListViewItem Item = new(inputBoxes.Select(box => box.Text).ToArray());
+
+            if (listView1.SelectedItems.Count == 0)
+            {
+                listView1.Items.Add(Item);
+                return;
+            }
+
+            listView1.Items.Insert(listView1.SelectedIndices[0], Item);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            MessageBox.Show($"{tabControl1.SelectedIndex} {tabControl1.SelectedTab}");
+
+            foreach (ListViewItem Item in listView1.SelectedItems)
+            {
+                var SubItems = Item.SubItems;
+
+                foreach (var (textBox, i) in inputBoxes.Select((v, i) => (v, i)))
+                {
+                    if (i < SubItems.Count)
+                    {
+                        SubItems[i].Text = textBox.Text;
+                        continue;
+                    }
+
+                    SubItems.Add(textBox.Text);
+                }
+            }
+        }
+
+        private void listView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            var SubItems = e.Item.SubItems;
+
+            foreach (var (textBox, i) in inputBoxes.Select((v, i) => (v, i)))
+            {
+                if (i < SubItems.Count)
+                {
+                    textBox.Text = SubItems[i].Text;
+                    continue;
+                }
+
+                textBox.Text = "";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                return;
+            }
+            if (MessageBox.Show("Clear?", "Clear", MessageBoxButtons.OKCancel) == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            foreach (ListViewItem Item in listView1.SelectedItems)
+            {
+                Item.SubItems.Clear();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 0)
+            {
+                return;
+            }
+            if (MessageBox.Show("Delete?", "Delete", MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                return;
+            }
+
+            foreach (ListViewItem Item in listView1.SelectedItems)
+            {
+                listView1.Items.Remove(Item);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count == 0)
+            {
+                return;
+            }
+            if (MessageBox.Show("Purge?", "Purge", MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                return;
+            }
+
+            listView1.Items.Clear();
         }
     }
 }
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,3 +1,4 @@
+using System.Collections.ObjectModel;
 using System.Windows;
 
 namespace WpfApp1
@@ -7,21 +8,117 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Employees Emps;
         public MainWindow()
         {
             InitializeComponent();
+
+            Emps = new();
+            DataContext = Emps;
         }
 
-        private void Button1_Click(object sender, RoutedEventArgs e)
+        private void Add_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show($"index: {TabControl.SelectedIndex}\nitem: {TabControl.SelectedItem}\n" +
-                $"value: {TabControl.SelectedValue}\ncontent: {TabControl.SelectedContent}");
+            Employee Emp = new(TextBox1.Text, TextBox2.Text);
+
+            if (ListView.SelectedItems.Count == 0)
+            {
+                Emps.Add(Emp);
+                return;
+            }
+
+            Emps.Insert(ListView.SelectedIndex, Emp);
         }
 
-        private void Button2_Click(object sender, RoutedEventArgs e)
+        private void Modify_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show($"index: {TabControl.SelectedIndex}\nitem: {TabControl.SelectedItem}\n" +
-                $"value: {TabControl.SelectedValue}\ncontent: {TabControl.SelectedContent}");
+            foreach (Employee Item in ListView.SelectedItems)
+            {
+                Item.Name = $"{TextBox1.Text} {TextBox2.Text}";
+            }
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListView.SelectedItems.Count == 0)
+            {
+                return;
+            }
+            if (MessageBox.Show("Clear?", "Clear", MessageBoxButton.OKCancel) == MessageBoxResult.Cancel)
+            {
+                return;
+            }
+
+            foreach (Employee Item in ListView.SelectedItems)
+            {
+                Item.Name = "";
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListView.SelectedItems.Count == 0)
+            {
+                return;
+            }
+            if (MessageBox.Show("Delete?", "Delete", MessageBoxButton.YesNo) == MessageBoxResult.No)
+            {
+                return;
+            }
+
+            foreach (Employee Item in ListView.SelectedItems)
+            {
+                ListView.Items.Remove(Item);
+            }
+        }
+
+        private void Purge_Click(object sender, RoutedEventArgs e)
+        {
+            if (ListView.SelectedItems.Count == 0)
+            {
+                return;
+            }
+            if (MessageBox.Show("Delete?", "Delete", MessageBoxButton.YesNo) == MessageBoxResult.No)
+            {
+                return;
+            }
+
+            Emps.Clear();
+        }
+    }
+
+    public class Employees : ObservableCollection<Employee>
+    {
+        public Employees() : base()
+        {
+            Add(new("hello", "world"));
+            Add(new("foo", "bar"));
+            Add(new("Kim", "John"));
+            Add(new("김", "철"));
+        }
+    }
+    public class Employee
+    {
+        public Employee(string first, string last) => (FirstName, LastName) = (first, last);
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private string? name;
+        public string Name
+        {
+            get => string.IsNullOrEmpty(name) ? $"{FirstName} {LastName}" : name;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    (FirstName, LastName, name) = ("", "", "");
+                    return;
+                }
+
+                name = value;
+
+                string[] names = value.Split(' ');
+                (FirstName, LastName) = names.Length == 2 ? (names[0], names[1]) : ("", "");
+            }
         }
     }
 }
